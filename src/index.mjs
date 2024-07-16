5bb7b7396474cadc1abbfcd87d045c54eaa85293
safi <safiou102003@gmail.com>
2024-07-16 12:11:46 -0700

    feat: get Requests

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,28 @@
-import express from "express";
-const app =express();
-const port =process.env.port ||3000
-app.listen(port,()=>{
-    console.log(`Running on port ${port}`);
+import express, { request, response } from "express";
+const app = express();
+const port = process.env.port || 3000;
+app.get("/", (request, response) => {
+  response.send("hello woorld ");
+});
+app.get("/api/users", (request, response) => {
+  response.status("201").send([
+    {
+      id: 1,
+      name: "safi",
+      work: "freelance",
+    },
+    {
+      id: 2,
+      name: "noor",
+      work: "dev",
+    },
+  ]);
+});
+
+app.get("/api/products",(request,response)=>{
+response.send([{id:1,name:'chicken'}])
 })
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
