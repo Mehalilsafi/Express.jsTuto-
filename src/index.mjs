a21100041c303588ad42826cff17f58afdb13831
safi <safiou102003@gmail.com>
2024-07-21 07:56:58 -0700

    feat : post request

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,8 @@
 import express, { request, response } from "express";
 const app = express();
 const port = process.env.port || 3000;
+
+// get requests
 app.get("/", (request, response) => {
   response.send("hello woorld ");
 });
@@ -16,6 +18,9 @@ const usersArray = [
     work: "dev",
   },
 ];
+
+
+//query paremters 
 app.get("/api/users", (request, response) => {
   console.log(request.query);
   const {
@@ -28,6 +33,14 @@ app.get("/api/users", (request, response) => {
 
   response.status("201").send(usersArray);
 });
+
+// post requests 
+app.post("/api/users",(request,response)=>{
+  console.log(request.body)
+  response.send(200)
+})
+
+//rout params 
 app.get("/api/users/:id", (request, response) => {
   const paresedInt = parseInt(request.params.id);
   if (isNaN(paresedInt)) {
