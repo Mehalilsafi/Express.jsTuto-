f08c3ac71667121968bbfcd0e5ce7db8273b54c6
safi <safiou102003@gmail.com>
2024-07-27 10:14:01 -0700

    feat : routs for userRout

---
+++ src/routes/users.mjs
@@ -0,0 +1,98 @@
+import { Router } from "express";
+import { usersArray } from "../utils/constants.mjs";
+import { idReslover } from "../utils/middlewares.mjs";
+import express, { request, response } from "express";
+import { query, validationResult, matchedData, body } from "express-validator";
+const router = Router();
+
+// the query paramters
+router.get(
+  "/api/users",
+  [
+    //query take the query paramters
+    query("filter")
+      .optional()
+      .isString()
+      .isLength({ min: 3, max: 10 })
+      .withMessage("Filter must be a string between 3 and 10 characters"),
+    query("value").optional().isString().withMessage("Value must be a string"),
+  ],
+  (request, response) => {
+    console.log(request.body);
+    // validationResult cantain the errors of the validtion
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      query: { filter, value },
+    } = request;
+    if (!filter && !value) return response.send(usersArray);
+    if (filter && value) {
+      response.send(usersArray.filter((user) => user[filter].includes(value)));
+    }
+
+    response.status("201").send(usersArray);
+  }
+);
+
+// post requests
+router.post(
+  "/api/users",
+  body("name")
+    .isString()
+    .withMessage("must be a string ")
+    .isEmpty()
+    .withMessage("must not be Empty "),
+  (request, response) => {
+    const error = validationResult(request);
+    if (!error.isEmpty()) {
+      return response.status(400).send({ errors: error.array });
+    }
+    const data = matchedData(request);
+
+    const newId =
+      usersArray.length > 0 ? usersArray[usersArray.length - 1].id + 1 : 1;
+    const newUser = {
+      id: newId,
+      ...data,
+    };
+    usersArray.push(newUser);
+    response.status(201).send(usersArray);
+  }
+);
+
+router.get("/api/users/:id", (request, response) => {
+  const paresedInt = parseInt(request.params.id);
+  if (isNaN(paresedInt)) {
+    response.status(400).send("bad request");
+  }
+  const dynamicUser = usersArray.find((user) => user.id === paresedInt);
+  if (!dynamicUser) {
+    return response.status(404).send("not found bad request");
+  } else {
+    return response.send(dynamicUser);
+  }
+});
+
+//put request
+router.patch("/api/users/:id", idReslover, (request, response) => {
+  console.log(request.body);
+  const { body, findIndexUser } = request;
+  //override the usersArray[findIndexUser] with ...body request
+  usersArray[findIndexUser] = { ...usersArray[findIndexUser], ...body };
+  return response.status(200).send(usersArray[findIndexUser]);
+});
+
+//delete
+router.delete("/api/users/:id", idReslover, (request, response) => {
+  const { findIndexUser } = request;
+
+  const userIndex = findIndexUser;
+  const deletedUser = usersArray.splice(userIndex, 1)[0];
+  return response.status(200).send(deletedUser);
+});
+
+
+export default router
