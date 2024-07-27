84d806384379fcb628b6c09dc3ca5f17718faed5
safi <safiou102003@gmail.com>
2024-07-27 09:44:08 -0700

    feat : middlwares in utils

---
+++ src/utils/middlewares.mjs
@@ -0,0 +1,20 @@
+const idReslover = (request, response, next) => {
+    const {
+      params: { id },
+    } = request;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      response.status(404).send("bad request ");
+    }
+    const findIndexUser = usersArray.findIndex((user) => user.id === parsedId);
+    if (findIndexUser === -1) {
+      response.status(404);
+    }
+    request.findIndexUser = findIndexUser;
+    next();
+  };
+  const loggingMiddleware = (request, response, next) => {
+    console.log(`${request.method} -- ${request.url}`);
+    next();
+  };
+  export {idReslover}
