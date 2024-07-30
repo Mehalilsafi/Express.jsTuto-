5df7c6c1a498e77d8afad1394a6d8fcfa077c3f3
safi <safiou102003@gmail.com>
2024-07-30 11:08:46 -0700

    feat : cookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,17 @@
 import express, { request, response } from "express";
 import userRouter from "./routes/users.mjs";
 import product from "./routes/product.mjs";
+import cookieParser from "cookie-parser";
 const app = express();
 const port = process.env.port || 3000;
 app.use(express.json());
+app.use(cookieParser())
 app.use(userRouter);
 app.use(product);
-
+app.get("/",(request,response)=>{
+  request.cookies()
+  response.send({msg:"hello world"})
+})
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
