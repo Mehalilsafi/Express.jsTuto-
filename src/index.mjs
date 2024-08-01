de3c20aa309ab345d9f5aaf0a18df8363c74b9f4
safi <safiou102003@gmail.com>
2024-08-01 10:57:27 -0700

    feat :express- session

--- src/index.mjs
+++ src/index.mjs
@@ -2,10 +2,12 @@ import express, { request, response } from "express";
 import userRouter from "./routes/users.mjs";
 import product from "./routes/product.mjs";
 import cookieParser from "cookie-parser";
+import { Session } from "express-session";
 const app = express();
 const port = process.env.port || 3000;
 app.use(express.json());
 app.use(cookieParser())
+app.use(Session())
 app.use(userRouter);
 app.use(product);
 app.get("/",(request,response)=>{
@@ -15,3 +17,10 @@ app.get("/",(request,response)=>{
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
+//  session are created on the server with an objct cantaine userId 
+//server response the with session id the browser 
+// browser save session id =abc123
+ //allow the brwoser to send the cookie in sub squent request 
+ //server pares the ckie frm text to jason with cookie parser 
+ //base oon the cooki the server with know who send request 
+ //remmbre http is statles 
