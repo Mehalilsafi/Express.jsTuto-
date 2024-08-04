09da2b4652f9ed93328c6725e19c68f225597580
safi <safiou102003@gmail.com>
2024-08-04 11:06:32 -0700

    feat:passport.js authenttication

--- src/index.mjs
+++ src/index.mjs
@@ -5,6 +5,7 @@ import { query, validationResult, matchedData, body } from "express-validator";
 import cookieParser from "cookie-parser";
 import session from "express-session";
 import { usersArray } from "./utils/constants.mjs";
+import passport from "passport"
 const app = express();
 const port = process.env.port || 3000;
 app.use(express.json());
@@ -19,54 +20,11 @@ app.use(
     },
   })
 );
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(userRouter);
 app.use(product);
-app.get("/", (request, response) => {
-  console.log(request.session.id);
-  console.log(request.session.id);
-  request.session.visited = true;
-  response.cookie("hello", "world", { maxAge: 60000*600 }); // Set a cookie
-  response.send({ msg: "hello world" }); // Send a response
-});
 
-app.post(
-  "/api/auth",
-  [
-    body("name")
-      .isString()
-      .withMessage("must be a string")
-      .notEmpty()
-      .withMessage("must not be empty"),
-    body("work")
-      .isString()
-      .withMessage("must be a string")
-      .notEmpty()
-      .withMessage("must not be empty"),
-    body("password")
-      .isString()
-      .withMessage("Password must be a string")
-      .notEmpty()
-      .withMessage("Password must not be empty"),
-  ],
-  (request,response) => {
-    const {
-      body: { name, password },
-    } = request;
-    const findUser = usersArray.find((user) => user.name === name);
-    if (!findUser) {
-      return response.status(401).send("user not found ");
-    }
-    const ispasswordCorrect =findUser.password=== password
-    if(!ispasswordCorrect){
-      return response.status(401).send("wrong password")
-    }
-    request.session.user = findUser;
-    return response.status(200).send(findUser);
-  }
-);
-app.get("/api/auth/status",(request,response)=>{
-  return  request.session.user ? response.status(200).send(request.session.user):response.status(401).send("wrong information")
-})
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
