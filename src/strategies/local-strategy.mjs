b3edc894241d6c952d4fe14e337c86cdc2db6059
safi <safiou102003@gmail.com>
2024-08-05 11:45:08 -0700

    feat:serializUser & desrialzeUser

--- src/strategies/local-strategy.mjs
+++ src/strategies/local-strategy.mjs
@@ -1,8 +1,21 @@
 import passport from "passport";
 import { Strategy } from "passport-local";
 import { usersArray } from "../utils/constants.mjs";
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+  try {
+    const findUser = usersArray.find((user) => user.id === id);
+    if (!findUser) throw new Error("User Nt Found");
+    done(null,findUser);
+  } catch (err) {
+    done(err, null);
+  }
+});
+
 export default passport.use(
-  new Strategy({usernameField:'name'},(name, password, done) => {
+  new Strategy({ usernameField: "name" }, (name, password, done) => {
     console.log(`userName :${name}`);
     console.log(` password:${password}`);
     const findUser = usersArray.find((user) => user.name === name);
