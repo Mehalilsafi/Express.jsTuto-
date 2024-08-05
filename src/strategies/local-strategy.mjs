b25e7538579496e982a4a2854bd753e2027de7ed
safi <safiou102003@gmail.com>
2024-08-05 09:50:16 -0700

    feat:validate user by passport

--- src/strategies/local-strategy.mjs
+++ src/strategies/local-strategy.mjs
@@ -1,2 +1,19 @@
+
 import passport from "passport"
 import {Strategy} from "passport-local"
+import {usersArray} from "../utils/constants"
+export default passport.use(
+    new Strategy((name,password, done)=>{  
+  const findUser=usersArray.find((user)=>user.name===name)
+  try{
+
+      if(!findUser)throw new Error('user not found');
+      if(findUser.password !==password) throw new Error("incrrect password")
+      done(null,findUser)  
+    }catch(err){
+        done(err,null)
+
+    }
+
+    })
+)
