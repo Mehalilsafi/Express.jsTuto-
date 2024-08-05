49966d2e728bf45dc8af620e3950a383229ad804
safi <safiou102003@gmail.com>
2024-08-05 12:40:17 -0700

    feat:comment to explain

--- src/index.mjs
+++ src/index.mjs
@@ -34,10 +34,12 @@ app.post(
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
-//  session are created on the server with an objct cantaine userId
-//server response the with session id the browser
-// browser save session id =abc123
-//allow the brwoser to send the cookie in sub squent request
-//server pares the ckie frm text to jason with cookie parser
-//base oon the cooki the server with know who send request
-//remmbre http is statles
+// The session is created on the server with an object containing the userId.
+// The server responds with a session ID that the browser stores in a cookie.
+// The browser saves the session ID, e.g., sessionID=abc123, and sends this cookie with subsequent requests.
+// The server uses the cookie-parser middleware to parse the cookie from a string into a JavaScript object.
+// Based on the session ID in the cookie, the server identifies the user who sent the request.
+// Remember, HTTP is stateless, meaning each request is independent of others.
+// When you set req.session.visited = true, it stores this information in the session object on the first visit.
+// On subsequent requests, the same session ID (from the cookie) is sent, allowing the server to retrieve and maintain the session state.
+
--- src/strategies/local-strategy.mjs
+++ src/strategies/local-strategy.mjs
@@ -1,9 +1,12 @@
 import passport from "passport";
 import { Strategy } from "passport-local";
 import { usersArray } from "../utils/constants.mjs";
+
+//The serializeUser function decides what part of the user object should be stored in the session. In this case, you're storing the user's id.
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
+//When a request is made and the session ID is found, Passport.js uses this deserializeUser function to turn that session ID back into a full user object.
 passport.deserializeUser((id, done) => {
   try {
     const findUser = usersArray.find((user) => user.id === id);
